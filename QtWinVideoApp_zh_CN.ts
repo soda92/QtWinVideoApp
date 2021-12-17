b5b7c1f52ef74d5db40cf7644a727239394993ff
SodaCris <18463922396@163.com>
2021-12-17 17:54:28 +0800

    add ts file

---
+++ QtWinVideoApp_zh_CN.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="17"/>
+        <location filename="build/QtWinVideoApp_autogen/include/ui_mainwindow.h" line="104"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <location filename="build/QtWinVideoApp_autogen/include/ui_mainwindow.h" line="105"/>
+        <source>视频预览</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <location filename="build/QtWinVideoApp_autogen/include/ui_mainwindow.h" line="106"/>
+        <source>设备管理</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="77"/>
+        <location filename="build/QtWinVideoApp_autogen/include/ui_mainwindow.h" line="107"/>
+        <source>火警信息</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="82"/>
+        <location filename="build/QtWinVideoApp_autogen/include/ui_mainwindow.h" line="108"/>
+        <source>事件记录</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <location filename="build/QtWinVideoApp_autogen/include/ui_mainwindow.h" line="109"/>
+        <source>停止报警</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
